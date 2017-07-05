0f4d389f62e4eab8b582efc4b9556a60d3cb5d4f
Alex <ajhollid@gmail.com>
2017-07-04 19:11:57 -0700

    initial commit

---
+++ app/src/main/java/com/dmtaiwan/alexander/myapplication/MainActivity.java
@@ -0,0 +1,21 @@
+package com.dmtaiwan.alexander.myapplication;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+
+public class MainActivity extends AppCompatActivity implements MyAsyncTask.AsyncListener{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        MyAsyncTask myTask = new MyAsyncTask(this);
+        myTask.execute();
+    }
+
+    @Override
+    public void returnString(String myString) {
+        Log.i("Interface method: ", myString);
+    }
+}
---
+++ app/src/main/java/com/dmtaiwan/alexander/myapplication/MyAsyncTask.java
@@ -0,0 +1,31 @@
+package com.dmtaiwan.alexander.myapplication;
+
+import android.os.AsyncTask;
+
+/**
+ * Created by AlexThinkPad on 7/4/2017.
+ */
+
+public class MyAsyncTask extends AsyncTask<Void, Void, String> {
+
+    private AsyncListener listener;
+
+    public MyAsyncTask(AsyncListener listener) {
+        this.listener = listener;
+    }
+
+    @Override
+    protected String doInBackground(Void... voids) {
+        return "This is my Test String";
+    }
+
+    @Override
+    protected void onPostExecute(String result) {
+        super.onPostExecute(result);
+        listener.returnString(result);
+    }
+
+    public interface AsyncListener {
+        void returnString(String myString);
+    }
+}
